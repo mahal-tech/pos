f62960f7308f3d9555e155501a161b4357a44f93
jual ahmed <md.jual.ah@gmail.com>
2020-03-25 23:41:43 +0600

    added purchase listing page

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('purchase-listing', require('./components/PurchaseListing.vue').default);
+Vue.component('multiselect', require('./components/multiselect.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
